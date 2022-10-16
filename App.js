ff11883cc113273c20f46413e9b2a92906e6afeb
Muhammad-Aatir <93130235+Muhammad-Aatir@users.noreply.github.com>
2022-10-16 10:43:03 +0500

    Add files via upload
    Lab Work Chess Board Layout

---
+++ App.js
@@ -0,0 +1,9 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View ,SafeAreaView} from 'react-native';
+import { Component } from 'react';
+import Chessfunction from './Component/Chessfunction';
+export default function App() {
+  return (
+
+    <Chessfunction></Chessfunction>
+  );}
